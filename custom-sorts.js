92c74498004f6f48af013f4c69c571aced598c1e
Aaron <offxcxal.aaron@icloud.com>
2024-05-25 13:32:48 -0700

    commit

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,5 +1,10 @@
 function ageSort(users) {
-  // Your code here 
+  // SORT USERS BY NAME FROM YOUNGEST TO OLDEST 
+  // PLACE USERS AGES INTO NEW ARRAY
+  //    ITERATE THROUGH ARRAY OF OBJECTS GET AGES
+  //    SORT AGES USING SORT METHOD
+  //    RETURN FIRST NAMES OF USERS IN ORDER
+  
 }
 
 function oddEvenSort(arr) {
@@ -25,3 +30,36 @@ module.exports = [
   frequencySort,
   ageSort,
 ];
+
+
+  // for (let user of users) {
+
+  //   // First
+  //   if (!sort.length) {
+  //     sort[0] = user;
+  //   }
+
+  //   // Second 
+  //   else if (sort.length <= 1) {
+  //     if (user.age < sort[0].age) {
+  //       sort.unshift(user);
+  //     } else {
+  //       sort.push(user);
+  //     }
+  //   }
+
+  //   // [1 , 4 , 6, 7]   5
+
+  //   // Sorting Algorithm
+  //   else {
+  //     for (let i = 0; i < sort.length; i++) {
+  //       if (sort[i].age < user.age && sort[i + 1].age > user.age) {
+  //         for (let j = sort.length; j > i; j--) {
+  //           sort[j] = sort[j - 1];
+  //         }
+  //         sort[i + 1] = user;
+  //       }
+  //     }
+  //   }
+  // }
+  // return sort.map( el => el.firstName);
